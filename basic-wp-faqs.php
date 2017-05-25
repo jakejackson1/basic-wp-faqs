cd0fd5063f5537418a57a82a3c9843ad62a82b79
Jake Jackson <jake@gravitypdf.com>
2017-05-25 15:28:02 +1000

    Include our WP Plugin Header Comment to register our plugin.

---
+++ basic-wp-faqs.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Basic WP FAQs
+Plugin URI:  https://github.com/jakejackson1/basic-wp-faqs
+Description: A basic FAQs plugin for WordPress. This plugin was written for the June 2017 WP Port Macquarie Meetup.
+Version:     0.1
+Author:      Jake Jackson
+Author URI:  https://github.com/jakejackson1
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: basic-wp-faqs
+*/
